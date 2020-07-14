47a096af6144f8345bb1f555f7c68db8360086a7
MarianVytak <marianvytak@gmail.com>
2020-07-14 15:19:40 +0300

    Commodity Card: added Featured.

--- src/templates/commodity-card/commodity-card.js
+++ src/templates/commodity-card/commodity-card.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+// Slider big.
 $('.commodity-card__slider-big').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -11,6 +13,7 @@ $('.commodity-card__slider-big').slick({
     asNavFor: '.commodity-card__slider-small'
 });
 
+// Slider small.
 $('.commodity-card__slider-small').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -21,3 +24,12 @@ $('.commodity-card__slider-small').slick({
     dots: false,
     asNavFor: '.commodity-card__slider-big'
 });
+
+
+// Featured.
+$('.commodity-card__featured_item').on('click', function (e) {
+    e.preventDefault();
+    let thisFeatured = $(this).attr('data-target');
+    $(this).removeClass('active');
+    $(thisFeatured).addClass('active');
+})
