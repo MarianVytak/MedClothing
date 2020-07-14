828874d4e5a540da69c7ccbb201df598f910c82c
MarianVytak <marianvytak@gmail.com>
2020-07-14 20:31:53 +0300

    Added read-more. Commodity Card: added description.

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -1,1 +1,24 @@
-$(function () {});
+$(function () {
+
+    'use strict';
+
+    // Read More
+    const readMoreLink = $('.read-more-link');
+    readMoreLink.on('click', function(e){
+        e.preventDefault();
+        let readMore = $(this).attr('href');
+        let readMoreContent = $(readMore + ' ' + '.read-more-content');
+        let text = $(this).text();
+
+        if (text === 'Развернуть') {
+            $(this).text('Свернуть');
+        } else {
+            $(this).text('Развернуть');
+        }
+
+        $(this).toggleClass('active');
+        $(readMore).toggleClass('active');
+        $(readMoreContent).fadeToggle();
+    });
+
+});
